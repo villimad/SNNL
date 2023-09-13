a421dc204a1ac37959e87e6ecc584b973943826b
home <home@mail.com>
2023-09-13 19:39:13 +0300

    CMake mod

---
+++ headers/layer.cpp
@@ -0,0 +1,6 @@
+#include "./layer.hpp"
+#include <iostream>
+
+void Layer::printh() {
+    std::cout << "Hey" << std::endl;
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "layer.hpp"
 
 int main(void) {
     std::cout << "Hello, World!" << std::endl;
+    Layer k{};
+    k.printh();
     return 0;
 }
